5b4bcc1e7ed28c4b13e9b78caf904fe6708f2f52
endrome <49007820+endrome@users.noreply.github.com>
2019-08-27 09:27:50 +1000

    Week 5

---
+++ Week 5/todo-list/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'To-Do List';
+    todos = [
+
+  ];
+
+  addTodo(newTodoLabel) {
+    var newTodo = {
+      label: newTodoLabel,
+      };
+    this.todos.push(newTodo);
+  }
+
+  deleteTodo(todo) {
+    this.todos = this.todos.filter( t => t.label !== todo.label );    
+  }
+};
